45c7bfeeb123c347b9499b566b2e7851f821a912
Ava Barron <mztriz@gmail.com>
2018-08-20 19:12:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 function theBeatlesPlay(musicians, instruments) {
   const array = []
 
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  for (let counter = 0, length = musicians.length; counter < length; counter++) {
+    array.push(`${musicians[counter]} plays ${instruments[counter]}`)
   }
   return array
 }
