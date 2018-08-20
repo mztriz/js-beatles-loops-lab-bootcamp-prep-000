ba99ab9d6849f4926c520b846199afe4d62b5de1
Ava Barron <mztriz@gmail.com>
2018-08-20 19:12:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function theBeatlesPlay(musicians, instruments) {
   for (let i = 0, l = musicians.length; i < l; i++) {
     array.push(`${musicians[i]} plays ${instruments[i]}`)
   }
-  return arr
+  return array
 }
