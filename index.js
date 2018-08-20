ea9710ceaa311f3a155bc64cae494ea6a646fe99
Ava Barron <mztriz@gmail.com>
2018-08-20 19:10:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // add solution here
 function theBeatlesPlay(musicians,instruments){
   var array = []
-  for (musician in musicians)
+  for (musician in musicians){
+    console.log(`${musician} plays`)
+  }
+  for (instrument in instruments){
+    console.log(`${instrument}`)
+  }
   
 }
